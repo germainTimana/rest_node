7a1ec2bd9c6eaf215ed3a096cb7a75802eba7a0d
germainTimana <germaintimana@gmail.com>
2020-10-29 08:57:15 -0500

    Crud server node 001

--- src/controlador/index-control-db.js
+++ src/controlador/index-control-db.js
@@ -1,36 +1,83 @@
 //importacion conexiones bases de datos
-const { Pool,Query } = require('pg');
+const { request } = require('express');
+const { Pool, Query } = require('pg');
 
 const pool = new Pool({
 host: 'localhost',
 user: 'postgres',
 password:'root',
-database: 'data001',
+database: 'citas',
 port: '5432'
 
 });
 
-const getUsers = async (req, res)=> {
-    const response = await pool.query('SELECT * FROM users');
-    res.status(200).json(response.rows);
-    console.log(response.rows);
+const getUsers = async (req,res)=> {
 
-    //res.send('se optienen Usuarios');
+    try {
+        const response = await pool.query('SELECT * FROM users');
+        res.status(200).json(response.rows);
+        //res.json({msg:'mensaje de conexion exitosa....'})
+        console.log(response.rows);
+
+        //res.send('se optienen Usuarios');
+    } catch (e) {
+        res.send(e);
+        console.log(e)
+    }
+};
+
+const getUserByid = async (req, res) => {
+    try {
+        
+        const response = await pool.query('SELECT * FROM users where id = $1', [req.params.param]);
+        if (response.rowCount==0) {
+            console.log(res.send("Usuario no encontrado"));
+        }
+        res.status(200).json(response.rows);
+        //res.json({msg:'mensaje de conexion exitosa....'})
+        //res.send("Usuario por ID");
+    } catch (e) {
+        res.send( e.detail);
+        console.log(e.detail); 
+    }
 };
 
 const createUsers = async (req, res)=> {
-    const {name, email} = req.body;
-    const response = await pool.query( 'INSERT INTO users (name , email) VALUES ($1 , $2)',[name, email]); 
+    try {
+        const {name, email} = req.body;
+    const response = await pool.query( 'INSERT INTO users (name , email) VALUES ($1 , $2)',[req.body.name, req.body.email]); 
     res.send('usuario creado....');
     console.log(response);
-
     //res.send('se crean Usuarios,,', [name, email]);
+    } catch (e) {
+        res.send( e.detail);
+        console.log(e.detail);
+    }
 };
 
-
+const updateUser = async (req, res) =>{
+    const id = req.params.param;
+    try {
+        const response = await pool.query('UPDATE users SET name =$1 , email= $2 WHERE id=$3',
+                                        [req.body.name, req.body.email, id]);
+        
+        if (response.rowCount==0) {
+            res.send('usuario No encontrado....');
+        }else{
+            res.send('usuario Actualizado....');
+        }
+        
+        console.log(response);
+   } catch (e) {
+        res.send( e.detail);
+        console.log(e.detail);
+   }
+};
 
 
 module.exports = {
     getUsers,
-    createUsers
+    createUsers,
+    getUserByid,
+    updateUser
 }
--- src/rutas/index-rutas.js
+++ src/rutas/index-rutas.js
@@ -1,10 +1,15 @@
 
 const { Router }= require('express');
 const router = Router();
-const { getUsers,createUsers} = require('../controlador/index-control-db')
+const { getUsers,createUsers,getUserByid, updateUser} = require('../controlador/index-control-db')
 
+router.get('/',(req, res) =>{
+    console.log('funcional...')
+    res.send('Biemvenido!!!')
+})
 router.get('/users', getUsers);
+router.get('/users/:param', getUserByid);
 router.post('/create-users', createUsers);
-
+router.put('/update-user/:param',updateUser)
 
 module.exports = router;
